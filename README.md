6db81eda91fab3ca6635e71f25d321fc0fe4ede7
Bsdpunk <bsdpunk@gmail.com>
2016-11-04 23:09:43 -0500

    update

---
+++ README.md
@@ -0,0 +1,4 @@
+##Create Time Results
+```
+for i in {1..5}; do bash time.sh ; done
+```
--- time.sh
+++ time.sh
@@ -1,3 +1,24 @@
 
-HUNDRED=$(for i in {1..100}; do printf "$RANDOM "; done)
-{ time node bubble.js $HUNDRED; } 2>&1 | grep real
+HUNDRED=$(for i in {1..1000}; do printf "$RANDOM "; done)
+
+
+
+printf "Node," >> time.log
+{ time node bubble.js $HUNDRED; } 2>&1 | grep real | grep -o '[0-9]\+\.[0-9]\+'  >> time.log
+
+
+printf "R," >> time.log
+    { time Rscript bubble.R $HUNDRED; } 2>&1 | grep real | grep -o '[0-9]\+\.[0-9]\+' >> time.log
+
+
+HUNDRED=$(for i in {1..1000}; do printf "$RANDOM "; done)
+    
+
+printf "R," >> time.log
+{ time Rscript bubble.R $HUNDRED; } 2>&1 | grep real | grep -o '[0-9]\+\.[0-9]\+' >> time.log
+
+
+printf "Node," >> time.log
+    { time node bubble.js $HUNDRED; } 2>&1 | grep real | grep -o '[0-9]\+\.[0-9]\+' >> time.log
+
+
